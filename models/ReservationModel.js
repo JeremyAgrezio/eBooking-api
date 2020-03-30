277682c484624c6ce43884f12bc3cf49af1e5a99
Jeremy Agrezio <jeremy.agrezio@orange.fr>
2020-03-30 22:58:50 +0200

    Add publication reference to reservation model

--- models/ReservationModel.js
+++ models/ReservationModel.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const Publication = require("../models/PublicationModel");
 
 const Schema = mongoose.Schema;
 
 const ReservationSchema = new Schema({
-    publication: {type: Schema.ObjectId, required: true},
+    publication: {type: Schema.ObjectId, required: true, ref: 'Publication'},
     tenant: {type: Schema.ObjectId, ref: "User", required: true},
 }, {timestamps: true});
 
