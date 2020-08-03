fbf82784681cf377c0029b55ea0256947ef2e680
Jérémy Agrezio <jeremy.agrezio@orange.fr>
2020-08-03 21:40:20 +0200

    [FEAT] Add 'open' & 'close' routes, remove obsolete

--- routes/lock.js
+++ routes/lock.js
@@ -4,10 +4,11 @@ const LockController = require("../controllers/LockController");
 const router = express.Router();
 
 router.get("/", LockController.lockList);
+router.post("/open", LockController.lockOpen);
+router.post("/close", LockController.lockClose);
 router.get("/:id", LockController.lockDetail);
 router.post("/", LockController.lockRegister);
 router.put("/:id", LockController.lockUpdate);
 router.delete("/:id", LockController.lockDelete);
-router.get("/", LockController.lockOpen);
 
 module.exports = router;
