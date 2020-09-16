0cfac8c696381058f735be60d83d23e19c3046c9
Jérémy Agrezio <jeremy.agrezio@orange.fr>
2020-09-16 21:10:28 +0200

    [DEV] JWT middleware refacto

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,26 +1,29 @@
 let jwt = require('jsonwebtoken')
 const secret = process.env.JWT_SECRET;
+const apiResponse = require("../helpers/apiResponse");
 
 const validateToken = (req, res, next) => {
-		const authorizationHeaader = req.headers.authorization;
+		const authorizationHeader = req.headers.authorization;
 		let result;
-		if (authorizationHeaader) {
+		if (authorizationHeader) {
 			const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
 			const options = {
 				expiresIn: '1d',
+				maxAge: '1d',
 			};
-			try {
-				// verify makes sure that the token hasn't expired and has been issued by us
-				result = jwt.verify(token, secret, options);
 
-				// Let's pass back the decoded token to the request object
-				req.user = result;
-				// We call next to pass execution to the subsequent middleware
-				next();
-			} catch (err) {
-				// Throw an error just in case anything goes wrong with verification
-				throw new Error(err);
-			}
+			// verify makes sure that the token hasn't expired and has been issued by us
+			jwt.verify(token, secret, options, function (err, decoded){
+				if (err) {
+					apiResponse.ErrorResponse(res, err.message);
+				} else {
+					// Let's pass back the decoded token to the request object
+					req.user = decoded;
+
+					// We call next to pass execution to the subsequent middleware
+					next();
+				}
+			});
 		} else {
 			result = {
 				error: `Authentication error. Token required.`,
